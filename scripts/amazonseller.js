b119dc150a55d1eb8fff4f17910b604a3de045c9
gunaasin <guna.asin06@gmail.com>
2025-02-01 19:59:40 +0530

    maodify the fashion , jewels

--- scripts/amazonseller.js
+++ scripts/amazonseller.js
@@ -21,6 +21,7 @@ document.querySelector('.app').innerHTML = `
           <option value="Smartphones">Smartphones</option>
           <option value="home essentials">Home Essentials</option>
           <option value="clothing">Clothing</option>
+          <option value="fashion , jewels">fashion , jewels</option>
           <option value="books">Books</option>
           <option value="home">Home & Kitchen</option>
           <option value="toys">Toys & Games</option>
